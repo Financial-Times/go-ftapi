9a2e77d797ac644d734f6e9c0f9f6c9a15e66afb
David Linton <david.linton@ft.com>
2016-02-09 16:49:47 +0000

    helpful ontology constants

--- helper.go
+++ helper.go
@@ -2,14 +2,30 @@ package ftapi
 
 import "regexp"
 
+type ontology struct {
+	Organisation   string
+	Person         string
+	Brand          string
+	Article        string
+	Mentions       string
+	IsClassifiedBy string
+}
+
+var Ontology = ontology{Organisation: "http://www.ft.com/ontology/organisation/Organisation",
+	Person:         "http://www.ft.com/ontology/person/Person",
+	Brand:          "http://www.ft.com/ontology/product/Brand",
+	Article:        "http://www.ft.com/ontology/content/Article",
+	Mentions:       "http://www.ft.com/ontology/annotation/mentions",
+	IsClassifiedBy: "http://www.ft.com/ontology/annotation/isClassifiedBy",
+}
+
 var UUIDRegexp = regexp.MustCompile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}")
 var FinalUUIDRegexp = regexp.MustCompile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$")
 
 func UUID(url string) string {
-    return UUIDRegexp.FindString(url)
+	return UUIDRegexp.FindString(url)
 }
 
 func FinalUUID(url string) string {
-    return FinalUUIDRegexp.FindString(url)
+	return FinalUUIDRegexp.FindString(url)
 }
-
