59822776921c44e9f39db37b746db5f8aef83576
David Linton <david.linton@ft.com>
2016-02-10 12:59:21 +0000

    raw reads like notifications

--- article.go
+++ article.go
@@ -1,57 +1,61 @@
 package ftapi
 
 type Annotation struct {
-    APIURL     string   `json:"apiUrl"`
-    DirectType string   `json:"directType"`
-    ID         string   `json:"id"`
-    Predicate  string   `json:"predicate"`
-    PrefLabel  string   `json:"prefLabel"`
-    Type       string   `json:"type"`
-    Types      []string `json:"types"`
+	APIURL     string   `json:"apiUrl"`
+	DirectType string   `json:"directType"`
+	ID         string   `json:"id"`
+	Predicate  string   `json:"predicate"`
+	PrefLabel  string   `json:"prefLabel"`
+	Type       string   `json:"type"`
+	Types      []string `json:"types"`
 }
 
 type Article struct {
-    Annotations []Annotation  `json:"annotations"`
-    BodyXML   string     `json:"bodyXML"`
-    Brands    []string   `json:"brands"`
-    Byline    string     `json:"byline"`
-    ID        string     `json:"id"`
-    MainImage struct {
-        ID string        `json:"id"`
-    }                    `json:"mainImage"`
-    PublishedDate string `json:"publishedDate"`
-    RequestURL    string `json:"requestUrl"`
-    Title         string `json:"title"`
-    Type          string `json:"type"`
-    WebURL        string `json:"webUrl"`
-    Comments struct {
-        Enabled bool     `json:"enabled"`
-    }                    `json:"comments"`
+	Annotations []Annotation `json:"annotations"`
+	BodyXML     string       `json:"bodyXML"`
+	Brands      []string     `json:"brands"`
+	Byline      string       `json:"byline"`
+	ID          string       `json:"id"`
+	MainImage   struct {
+		ID string `json:"id"`
+	} `json:"mainImage"`
+	PublishedDate string `json:"publishedDate"`
+	RequestURL    string `json:"requestUrl"`
+	Title         string `json:"title"`
+	Type          string `json:"type"`
+	WebURL        string `json:"webUrl"`
+	Comments      struct {
+		Enabled bool `json:"enabled"`
+	} `json:"comments"`
+}
+
+func (a Article) UUID() string {
+	return FinalUUID(a.ID)
 }
 
 func (c *Client) ArticleByUUID(uuid string) (result *Article, err error) {
-    url := "https://api.ft.com/content/"+uuid
-    return c.Article(url)
+	url := "https://api.ft.com/content/" + uuid
+	return c.Article(url)
 }
 
 func (c *Client) EnrichedArticleByUUID(uuid string) (result *Article, err error) {
-    url := "https://api.ft.com/enrichedcontent/"+uuid
-    return c.Article(url)
+	url := "https://api.ft.com/enrichedcontent/" + uuid
+	return c.Article(url)
 }
 
 func (c *Client) EnrichedArticle(url string) (result *Article, err error) {
-    return c.EnrichedArticleByUUID(FinalUUID(url))
+	return c.EnrichedArticleByUUID(FinalUUID(url))
 }
 
 func (c *Client) Article(url string) (result *Article, err error) {
-    result = &Article{}
-    err = c.jsonAtURL(url, result)
-    return result, err
+	result = &Article{}
+	err = c.jsonAtURL(url, result)
+	return result, err
 }
 
 func (c *Client) MainImageSet(article *Article) (result *ImageSet, err error) {
-    if article.MainImage.ID == "" {
-        return nil, nil
-    }
-    return c.ImageSet(article.MainImage.ID)
+	if article.MainImage.ID == "" {
+		return nil, nil
+	}
+	return c.ImageSet(article.MainImage.ID)
 }
--- recommended-reads.go
+++ recommended-reads.go
@@ -1,72 +1,105 @@
 package ftapi
 
 import (
-        "net/url"
-        "strconv"
+	"net/url"
+	"strconv"
 )
 
 type Recommendation struct {
-    ID         string  `json:"id"`
-    Popularity float64 `json:"popularity"`
-    Published  string  `json:"published"`
-    Score      float64 `json:"score"`
-    Title      string  `json:"title"`
+	ID            string  `json:"id"`
+	Popularity    float64 `json:"popularity"`
+	PublishedDate string  `json:"published"`
+	Score         float64 `json:"score"`
+	Title         string  `json:"title"`
+	URL           string  `json:"url"`
 }
 
 type Recommendations struct {
-    Articles []Recommendation `json:"articles"`
-    Status  string `json:"status"`
-    Type    string `json:"type"`
-    Version string `json:"version"`
+	Articles []Recommendation `json:"articles"`
+	Status   string           `json:"status"`
+	Type     string           `json:"type"`
+	Version  string           `json:"version"`
 }
 
-func (c *Client) ContextualRecommendationsByUuid(uuid string, count int, recency int) (result *Recommendations, err error) {
-    u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/contextual")
-    q := u.Query()
-    q.Set("contentid", uuid)
-    q.Set("count", strconv.Itoa(count))
-    q.Set("sort", "rel")
-    q.Set("recency", strconv.Itoa(recency))
-    u.RawQuery = q.Encode()
-    result = &Recommendations{}
-    err = c.jsonAtURL(u.String(), result)
-    return result, err
+func (c *Client) RawContextualRecommendationsByUUID(uuid string, count int, recency int) (result *Recommendations, err error) {
+	u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/contextual")
+	q := u.Query()
+	q.Set("contentid", uuid)
+	q.Set("count", strconv.Itoa(count))
+	q.Set("sort", "rel")
+	q.Set("recency", strconv.Itoa(recency))
+	u.RawQuery = q.Encode()
+	result = &Recommendations{}
+	err = c.jsonAtURL(u.String(), result)
+	return result, err
 }
 
-func (c *Client) BehaviouralRecommendationsByUuid(uuid string, userid string, count int, recency int) (result *Recommendations, err error) {
-    u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/behavioural")
-    q := u.Query()
-    q.Set("contentid", uuid)
-    q.Set("userid", userid)
-    q.Set("count", strconv.Itoa(count))
-    q.Set("sort", "rel")
-    q.Set("recency", strconv.Itoa(recency))
-    u.RawQuery = q.Encode()
-    result = &Recommendations{}
-    err = c.jsonAtURL(u.String(), result)
-    return result, err
+func (c *Client) ContextualRecommendationsByUUID(uuid string, count int, recency int) (result []Recommendation, err error) {
+	r, err := c.RawContextualRecommendationsByUUID(uuid, count, recency)
+	if err != nil {
+		return nil, err
+	}
+	return r.Articles, nil
 }
 
-func (c *Client) BehaviouralRecommendations(userid string, count int, recency int) (result *Recommendations, err error) {
-    u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/behavioural")
-    q := u.Query()
-    q.Set("userid", userid)
-    q.Set("count", strconv.Itoa(count))
-    q.Set("sort", "rel")
-    q.Set("recency", strconv.Itoa(recency))
-    u.RawQuery = q.Encode()
-    result = &Recommendations{}
-    err = c.jsonAtURL(u.String(), result)
-    return result, err
+func (c *Client) RawBehaviouralRecommendationsByUUID(uuid string, userid string, count int, recency int) (result *Recommendations, err error) {
+	u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/behavioural")
+	q := u.Query()
+	q.Set("contentid", uuid)
+	q.Set("userid", userid)
+	q.Set("count", strconv.Itoa(count))
+	q.Set("sort", "rel")
+	q.Set("recency", strconv.Itoa(recency))
+	u.RawQuery = q.Encode()
+	result = &Recommendations{}
+	err = c.jsonAtURL(u.String(), result)
+	return result, err
 }
 
-func (c *Client) PopularRecommendations(count int, recency int) (result *Recommendations, err error) {
-    u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/popular")
-    q := u.Query()
-    q.Set("count", strconv.Itoa(count))
-    q.Set("recency", strconv.Itoa(recency))
-    u.RawQuery = q.Encode()
-    result = &Recommendations{}
-    err = c.jsonAtURL(u.String(), result)
-    return result, err
+func (c *Client) BehaviouralRecommendationsByUUID(uuid string, userid string, count int, recency int) (result []Recommendation, err error) {
+	r, err := c.RawBehaviouralRecommendationsByUUID(uuid, userid, count, recency)
+	if err != nil {
+		return nil, err
+	}
+	return r.Articles, nil
+}
+
+func (c *Client) RawBehaviouralRecommendations(userid string, count int, recency int) (result *Recommendations, err error) {
+	u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/behavioural")
+	q := u.Query()
+	q.Set("userid", userid)
+	q.Set("count", strconv.Itoa(count))
+	q.Set("sort", "rel")
+	q.Set("recency", strconv.Itoa(recency))
+	u.RawQuery = q.Encode()
+	result = &Recommendations{}
+	err = c.jsonAtURL(u.String(), result)
+	return result, err
+}
+
+func (c *Client) BehaviouralRecommendations(userid string, count int, recency int) (result []Recommendation, err error) {
+	r, err := c.RawBehaviouralRecommendations(userid, count, recency)
+	if err != nil {
+		return nil, err
+	}
+	return r.Articles, nil
+}
+
+func (c *Client) RawPopularRecommendations(count int, recency int) (result *Recommendations, err error) {
+	u, err := url.Parse("https://api.ft.com/recommended-reads-api/recommend/popular")
+	q := u.Query()
+	q.Set("count", strconv.Itoa(count))
+	q.Set("recency", strconv.Itoa(recency))
+	u.RawQuery = q.Encode()
+	result = &Recommendations{}
+	err = c.jsonAtURL(u.String(), result)
+	return result, err
+}
+
+func (c *Client) PopularRecommendations(count int, recency int) (result []Recommendation, err error) {
+	r, err := c.RawPopularRecommendations(count, recency)
+	if err != nil {
+		return nil, err
+	}
+	return r.Articles, nil
 }
