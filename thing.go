f3ce1ae185e01522365712d3a42fe8316c1827eb
david.linton <david.linton@ft.com>
2018-03-26 17:12:38 +0100

    add concept search (without tests :blush:)

--- thing.go
+++ thing.go
@@ -1,5 +1,9 @@
 package ftapi
 
+import (
+    "net/url"
+)
+
 type Thing struct {
     RawJSON    *[]byte
     APIURL     string   `json:"apiUrl"`
@@ -15,6 +19,16 @@ type Concept struct {
     BroaderConcepts []Annotation `json:"broaderConcepts"`
     NarrowerConcepts []Annotation `json:"narrowerConcepts"`
     RelatedConcepts []Annotation `json:"relatedConcepts"`
+}
+
+type ConceptSearchResponse struct {
+    RawJSON *[]byte
+    Concepts []struct {
+        APIURL     string   `json:"apiUrl"`
+        DirectType string   `json:"type"`
+        ID         string   `json:"id"`
+        PrefLabel  string   `json:"prefLabel"`
+    } `json:"concepts"`
 }
 
 func (c *Client) ThingByUUID(uuid string) (result *Thing, err error) {
@@ -40,3 +54,28 @@ func (c *Client) Concept(url string) (result *Concept, err error) {
     result.RawJSON = raw
     return result, err
 }
+
+func (c *Client) ConceptSearch(s string) (result []Thing, err error) {
+    result = []Thing{}
+
+    u, _ := url.Parse("https://api.ft.com/concepts")
+    q := u.Query()
+    q.Set("mode", "search")
+    q.Set("q", s)
+    q.Set("type", Ontology.Topic)
+    u.RawQuery = q.Encode()
+    resp := &ConceptSearchResponse{}
+
+    _, err = c.FromURL(u.String(), resp)
+
+    for _, t := range resp.Concepts {
+        result = append(result, Thing{
+            APIURL: t.APIURL,
+            DirectType: t.DirectType,
+            ID: t.ID,
+            PrefLabel: t.PrefLabel,
+        })
+    }
+    return result, err
+}
+
